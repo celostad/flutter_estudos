e01e5122a1511ca892d3ae3e31381f889fe9990a
Marcelo Tadim <marcelo.tadim@lowcost.com.br>
2025-04-15 19:02:40 -0300

    [aula4] 05.Personalizando a Condição de dificuldade

--- lib/main.dart
+++ lib/main.dart
@@ -25,25 +25,29 @@ class MyApp extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(top: 8),
               child: Tasks(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'
-              ),
+                'Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                3),
             ),
             Tasks(
               'Andar de Bike',
-              'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+              2
             ),
             Tasks(
               'Ler',
-              'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'
+              'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+              4
             ),
             Tasks(
               'Meditar',
-              'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'
+              'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+              5
             ),
             Tasks(
               'Jogar',
-              'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'
+              'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
+              1
             )
           ],
         ),
@@ -58,8 +62,9 @@ class MyApp extends StatelessWidget {
 class Tasks extends StatefulWidget {
   final String nome;
   final String foto;
+  final int dificuldade;
 
-  const Tasks(this.nome,this.foto, {Key? key}) : super(key: key);
+  const Tasks(this.nome,this.foto, this.dificuldade, {Key? key}) : super(key: key);
 
   @override
   State<Tasks> createState() => _TasksState();
@@ -118,11 +123,31 @@ class _TasksState extends State<Tasks> {
                           ),
                           Row(
                             children: [
-                              Icon(Icons.star, size:15, color:Colors.blue,),
-                              Icon(Icons.star, size:15, color:Colors.blue,),
-                              Icon(Icons.star, size:15, color:Colors.blue,),
-                              Icon(Icons.star, size:15, color:Colors.blue[100],),
-                              Icon(Icons.star, size:15, color:Colors.blue[100],),
+                              Icon(
+                                Icons.star,
+                                size:15,
+                                color:(widget.dificuldade >= 1) ? Colors.blue : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size:15,
+                                color:(widget.dificuldade >= 2) ? Colors.blue : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size:15,
+                                color:(widget.dificuldade >= 3) ? Colors.blue : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size:15,
+                                color:(widget.dificuldade >= 4) ? Colors.blue : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size:15,
+                                color:(widget.dificuldade >= 5) ? Colors.blue : Colors.blue[100],
+                              ),
 
                             ],
                           ),
@@ -168,7 +193,9 @@ class _TasksState extends State<Tasks> {
                         width: 200,
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel/10,
+                          value: (widget.dificuldade > 0)
+                          ? (nivel/widget.dificuldade)/10
+                          : 1,
                         ),
                       ),
                     ),
